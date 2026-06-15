3e969e8b2ebb407e148f9f10bbabbe84346ef95f
arazelectron19 <ssx4077@gmail.com>
2026-06-15 17:59:29 +0400

    Add files via upload

---
+++ Zakaz qebul/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'zakaz-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Quraşdırılma və faylların keşe yığılması
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Oflayn rejimdə və ya sürətli açılış üçün keşdən oxumaq
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
